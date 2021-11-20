473873af7e5b3cbbefca7e361958e32b9b524f29
Peter Wilkins <peter.wilkins@ovoenergy.com>
2021-11-20 11:53:51 +0000

    flags

--- src/com/example/pathom.cljs
+++ src/com/example/pathom.cljs
@@ -3,56 +3,64 @@
    
    Add your resolvers and 'server-side' mutations here."
   (:require
-    [com.wsscode.pathom.core :as p]
-    [com.wsscode.pathom.connect :as pc]))
+    [com.wsscode.pathom.core :as pm]
+    [com.wsscode.pathom.connect :as pc]
+    [promesa.core :as p]))
 
-(pc/defresolver index-explorer 
+(pc/defresolver index-explorer
   "This resolver is necessary to make it possible to use 'Load index' in Fulcro Inspect - EQL"
   [env _]
-  {::pc/input  #{:com.wsscode.pathom.viz.index-explorer/id}
+  {::pc/input #{:com.wsscode.pathom.viz.index-explorer/id}
    ::pc/output [:com.wsscode.pathom.viz.index-explorer/index]}
   {:com.wsscode.pathom.viz.index-explorer/index
    (-> (get env ::pc/indexes)
        (update ::pc/index-resolvers #(into {} (map (fn [[k v]] [k (dissoc v ::pc/resolve)])) %))
        (update ::pc/index-mutations #(into {} (map (fn [[k v]] [k (dissoc v ::pc/mutate)])) %)))})
 
-(pc/defresolver i-fail 
-  [_ _]
-  {::pc/input  #{}
-   ::pc/output [:i-fail]}
-  (throw (ex-info "Fake resolver error" {})))
+(defn json-get [url]
+  (p/let [resp (js/fetch url)
+          json (.json resp)]
+    (js->clj json :keywordize-keys true)))
 
-(pc/defresolver person
-  [_ {id :person/id}]
-  {::pc/input  #{:person/id}
-   ::pc/output [:person/id :person/name]}
-  {:person/id id, :person/name (str "Joe #" id)})
+(defn all-countries* []
+  (->> (json-get "https://restcountries.com/v2/all?fields=name,capital,region,population,flags")
+       ))
 
-(pc/defmutation create-random-thing [env {:keys [tmpid] :as params}]
-  ;; Fake generating a new server-side entity with
-  ;; a server-decided actual ID
-  ;; NOTE: To match with the Fulcro-sent mutation, we
-  ;; need to explicitly name it to use the same symbol
-  {::pc/sym 'com.example.mutations/create-random-thing
-   ::pc/params [:tempid]
-   ::pc/output [:tempids]}
-  (println "SERVER: Simulate creating a new thing with real DB id 123" tmpid)
-  {:tempids {tmpid 123}})
+(comment
 
-(def my-resolvers-and-mutations 
+
+  (defonce state (atom nil))
+  (-> (all-countries*)
+      (.then #(reset! state %)))
+
+  @state
+
+  (->> @state
+       (map
+         (fn [{:keys [name capital region population flags]}]
+           {:country/name name :country/population population :country/capital capital :country/region region :country/flag flags})
+
+         ))
+  )
+
+(pc/defresolver all-countries [_]
+  {::pc/output [:countries]}
+  {:countries (all-countries*)})
+
+(def my-resolvers-and-mutations
   "Add any resolvers you make to this list (and reload to re-create the parser)"
-  [index-explorer create-random-thing i-fail person])
+  [index-explorer all-countries])
 
-(defn new-parser 
+(defn new-parser
   "Create a new Pathom parser with the necessary settings"
   []
-  (p/parallel-parser
-    {::p/env     {::p/reader [p/map-reader
-                              pc/parallel-reader
-                              pc/open-ident-reader]
-                  ::pc/mutation-join-globals [:tempids]}
-     ::p/mutate  pc/mutate-async
+  (pm/parallel-parser
+    {::p/env {::p/reader [pm/map-reader
+                          pc/parallel-reader
+                          pc/open-ident-reader]
+              ::pc/mutation-join-globals [:tempids]}
+     ::p/mutate pc/mutate-async
      ::p/plugins [(pc/connect-plugin {::pc/register my-resolvers-and-mutations})
-                  p/error-handler-plugin
-                  p/request-cache-plugin
-                  (p/post-process-parser-plugin p/elide-not-found)]}))
+                  pm/error-handler-plugin
+                  pm/request-cache-plugin
+                  (pm/post-process-parser-plugin pm/elide-not-found)]}))
--- src/com/example/ui.cljs
+++ src/com/example/ui.cljs
@@ -8,13 +8,18 @@
     [com.fulcrologic.fulcro.components :as comp :refer [defsc transact!]]
     [com.fulcrologic.fulcro.raw.components :as rc]
     [com.fulcrologic.fulcro.data-fetch :as df]    
-    [com.fulcrologic.fulcro.dom :as dom :refer [button div form h1 h2 h3 input label li ol p ul]]))
+    [com.fulcrologic.fulcro.dom :as dom :refer [button div form h1 h2 h3 input label li ol p ul img]]))
 
-(defsc CountryCard [this {:country/keys [name population capital region] }]
-  {:query [:country/name :country/population :country/capital :country/region]
+(defsc CountryCard [this {:country/keys [name population capital region flag] }]
+  {:query [:country/name :country/population :country/capital :country/region :country/flag]
    :initial-state (fn [{:country/keys [name population capital region]}]
                     [{:country/name name :country/population population :country/capital capital :country/region region}])}
-  (div (h1 name) (p "Population:" population) (p "Capital:" capital) (p "Region:" region))
+  (div
+    (img {:src flag})
+    (h1 name)
+    (p "Population:" population)
+    (p "Capital:" capital)
+    (p "Region:" region))
   )
 
 (def ui-contry-card (comp/factory CountryCard {:keyfn :country/name}))
@@ -22,7 +27,67 @@
 (defsc Root [this {:keys [countries]}]
   {:query [{:countries (comp/get-query CountryCard)}]
    :initial-state (fn [_]
-                    {:countries (comp/get-initial-state CountryCard {:country/name "uk" :country/population 3 :country/capital "bristol" :country/region "Europe"})})}
+                    {:countries [#:country{:name "United Kingdom of Great Britain and Northern Ireland",
+                                           :population 67215293,
+                                           :capital "London",
+                                           :region "Europe",
+                                           :flag "https://flagcdn.com/w320/gb.png"}
+                                 #:country{:name "United States of America",
+                                           :population 329484123,
+                                           :capital "Washington, D.C.",
+                                           :region "Americas",
+                                           :flag "https://flagcdn.com/w320/us.png"}
+                                 #:country{:name "Uruguay",
+                                           :population 3473727,
+                                           :capital "Montevideo",
+                                           :region "Americas",
+                                           :flag "https://flagcdn.com/w320/uy.png"}
+                                 #:country{:name "Uzbekistan",
+                                           :population 34232050,
+                                           :capital "Tashkent",
+                                           :region "Asia",
+                                           :flag "https://flagcdn.com/w320/uz.png"}
+                                 #:country{:name "Vanuatu",
+                                           :population 307150,
+                                           :capital "Port Vila",
+                                           :region "Oceania",
+                                           :flag "https://flagcdn.com/w320/vu.png"}
+                                 #:country{:name "Venezuela (Bolivarian Republic of)",
+                                           :population 28435943,
+                                           :capital "Caracas",
+                                           :region "Americas",
+                                           :flag "https://flagcdn.com/w320/ve.png"}
+                                 #:country{:name "Vietnam",
+                                           :population 97338583,
+                                           :capital "Hanoi",
+                                           :region "Asia",
+                                           :flag "https://flagcdn.com/w320/vn.png"}
+                                 #:country{:name "Wallis and Futuna",
+                                           :population 11750,
+                                           :capital "Mata-Utu",
+                                           :region "Oceania",
+                                           :flag "https://flagcdn.com/w320/wf.png"}
+                                 #:country{:name "Western Sahara",
+                                           :population 510713,
+                                           :capital "El Aaiún",
+                                           :region "Africa",
+                                           :flag "https://flagcdn.com/w320/eh.png"}
+                                 #:country{:name "Yemen",
+                                           :population 29825968,
+                                           :capital "Sana'a",
+                                           :region "Asia",
+                                           :flag "https://flagcdn.com/w320/ye.png"}
+                                 #:country{:name "Zambia",
+                                           :population 18383956,
+                                           :capital "Lusaka",
+                                           :region "Africa",
+                                           :flag "https://flagcdn.com/w320/zm.png"}
+                                 #:country{:name "Zimbabwe",
+                                           :population 14862927,
+                                           :capital "Harare",
+                                           :region "Africa",
+                                           :flag "https://flagcdn.com/w320/zw.png"}]
+})}
   (div "Where in the world"
        (map ui-contry-card countries)))
-        
+
